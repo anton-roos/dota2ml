8691b1e4b46aff55650ed9d140987b147124e997
Anton Roos <AntonR@hollard.co.za>
2025-02-25 11:52:18 +0200

    change parameters

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ X = np.array(X)
 y = np.array(y)
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Random Forest Classifier
 model = RandomForestClassifier()
